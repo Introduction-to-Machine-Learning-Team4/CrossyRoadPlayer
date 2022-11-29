12b54b9fabc6b6836740f635012c1f73b1e762dc
Bob <bobolai1113@gmail.com>
2022-11-29 21:03:47 +0800

    Revise env api usage
    Co-authored-by: Sean <ChenYuSean@users.noreply.github.com>

--- Modules/worker.py
+++ Modules/worker.py
@@ -20,6 +20,7 @@ class Worker(mp.Process):
         
         self.name = f'woker {name}'
         self.env = UnityEnvironment(file_name="CRML", seed=1, side_channels=[])
+        self.behavior = list(self.env.behavior_specs)[0]
     
     def pull(self, global_network):
         """
@@ -37,13 +38,25 @@ class Worker(mp.Process):
         self.l_ep = 0
         while self.global_network.value < NUM_GAMES:
             done = False
-            state = self.env.reset()
+            self.env.reset()
             score = 0
             self.local_network.reset()
             while not done:
+                decision_steps, terminal_steps = self.env.get_steps(self.behavior)
+                step = None
+                if len(terminal_steps) != 0:
+                    step = terminal_steps[terminal_steps.agent_id[0]]
+                    done = True
+                else:
+                    step = decision_steps[decision_steps.agent_id[0]]
+                state = step.obs ## Unity return
                 action = self.local_network.take_action(state)
-                # FIXME: not compatible with current unity env., som slight adjustment is needed
-                state_new, reward, done = self.env.set_actions(action) 
+                # FIXME: not compatible with current unity env., some slight adjustment is needed
+                actionTuple = ActionTuple()
+                actionTuple.add_discrete(action) ## please give me a INT in a 2d nparray!!
+                # state_new, reward, done = self.env.set_actions(action) 
+                self.env.set_actions(self.behavior, actionTuple)
+                reward = step.reward ## Unity return
                 score += reward
                 self.local_network.record(state, action, reward)
                 if self.l_ep % MAX_EP == 0 or done:
@@ -56,7 +69,8 @@ class Worker(mp.Process):
                     self.local_network.load_state_dict(self.global_network.state_dict()) # pull
                     # self.local_network.reset()
                 self.l_ep += 1
-                state = state_new
+                self.env.step()
+                # state = state_new
             with self.g_ep.get_losk():
                 self.g_ep.value += 1
-            print(self.name, 'episode ', self.episode_idx.value, 'reward %.1f' % score)
+            print(self.name, 'episode ', self.episode_idx.value, f'reward {score}')
